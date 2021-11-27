5c143c9282b7b615bd9ccf7f56ca96f77bbc1f87
ForceAwakenz <djimeilsobachka@gmail.com>
2021-11-27 19:15:23 +0200

    implement first ngrx reducer and action

---
+++ src/app/auth/action-types.ts
@@ -0,0 +1,3 @@
+import * as AuthActions from "./auth.acitons";
+
+export { AuthActions };
---
+++ src/app/auth/reducers/index.ts
@@ -0,0 +1,26 @@
+import { User } from "./../model/user.model";
+import {
+  ActionReducer,
+  ActionReducerMap,
+  createFeatureSelector,
+  createReducer,
+  createSelector,
+  MetaReducer,
+  on,
+} from "@ngrx/store";
+import { AuthActions } from "../action-types";
+
+export interface AuthState {
+  user: User;
+}
+
+export const initialAuthState: AuthState = { user: undefined };
+
+export const authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state, action) => {
+    return {
+      user: action.user,
+    };
+  })
+);
